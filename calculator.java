b23f1e6b0a79b19c4837c067fe952b213dd2cd08
Kevin Yu <118699283+KevinYu415@users.noreply.github.com>
2024-01-04 03:50:56 -0800

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+class calculator{
+    public static void main(String[] args){
+
+        
+
+        while(true){
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Please enter numbers of exercise and math symbol, or quit");
+            System.out.println("format example: 33 * ");
+            System.out.print("input: ");
+
+            String inputs = myObj.nextLine();  // Read user input
+            // System.out.println("Username is: " + inputs);  // Output user inputhi
+
+            if(inputs.equals("quit")){
+                break;
+            }
+
+            String[] arr = inputs.split("\s+");
+
+
+            // for(String i: arr){
+            //     System.out.println(i);
+            // }
+            
+            // max and min range
+            int max = 9;
+            int min = 2;
+            int correct = 0;
+            int incorrect = 0;
+            for(int i = 0; i<Integer.valueOf(arr[0]); i++){
+                // System.out.println(i + "yes");
+                int one = (int)(Math.random()*max)+min;
+                int two = (int)(Math.random()*max)+min;
+
+                if(arr[1].equals("*")){
+                    Scanner ans = new Scanner(System.in);
+                    System.out.print("What is " + one + " * " + two + " = ");
+                    String answer = ans.nextLine();
+                    if((one * two) == Integer.valueOf(answer)){
+                        System.out.println("correct \n");
+                        correct++;
+                    }else{
+                        System.out.println("Incorrect, should be " + one * two + "\n");
+                        incorrect++;
+                    }
+                }else if(arr[1].equals("+")){
+                    Scanner ans = new Scanner(System.in);
+                    System.out.print("What is " + one + " + " + two + " = ");
+                    String answer = ans.nextLine();
+                    if((one + two) == Integer.valueOf(answer)){
+                        System.out.println("correct \n");
+                        correct++;
+                    }else{
+                        System.out.println("Incorrect, should be " + one + two + "\n");
+                        incorrect++;
+                    }
+                }else if(arr[1].equals("-")){
+                    Scanner ans = new Scanner(System.in);
+                    System.out.print("What is " + one + " - " + two + " = ");
+                    String answer = ans.nextLine();
+                    int sub = one - two;
+                    if((sub) == Integer.valueOf(answer)){
+                        System.out.println("correct \n");
+                        correct++;
+                    }else{
+                        System.out.println("Incorrect, should be " + sub + "\n");
+                        incorrect++;
+                    }
+
+                    // division need to be fixed. currently converting double to int. losing data
+                }else if(arr[1].equals("/")){
+                    Scanner ans = new Scanner(System.in);
+                    System.out.print("What is " + one + " / " + two + " = ");
+                    String answer = ans.nextLine();
+                    if((one / two) == Integer.valueOf(answer)){
+                        System.out.println("correct \n");
+                        correct++;
+                    }else{
+                        System.out.println("Incorrect, should be " + one / two + "\n");
+                        incorrect++;
+                    }
+                }
+            }
+            System.out.println("You got " + correct +" correct , " + incorrect + " incorrect \n");
+        }
+        System.out.println("Finished");
+    }
+}
